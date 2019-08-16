368a70e20bf3152cb3232bba55aae50c4399ca62
Hangyu Ye <jerry.hangyu@gmail.com>
2019-08-16 22:19:21 +0800

    Initial Commit

---
+++ bitPocket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitPocket
+//
+//  Created by Jerry on 8/16/19.
+//  Copyright © 2019 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
